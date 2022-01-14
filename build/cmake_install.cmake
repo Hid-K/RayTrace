c27ee763792724d6de7295707668c35437e911f3
Hid-K <61284529+Hid-K@users.noreply.github.com>
2022-01-14 23:28:42 +0300

    changed cmake version to 3.2.0

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/laptevaleksander/Documents/Projects/RayTrace
+# Install script for directory: /Users/laptevaleksander/Documents/GitHub/RayTrace
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/laptevaleksander/Documents/Projects/RayTrace/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/laptevaleksander/Documents/GitHub/RayTrace/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
